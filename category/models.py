627d7373a38e53a31aa5f05040299d3227f2c33a
Aline Miranda <aline.lcmiranda@gmail.com>
2024-01-03 12:18:49 -0300

    product_detail add

--- category/models.py
+++ category/models.py
@@ -13,9 +13,9 @@ class Category(models.Model):
         verbose_name = 'category'
         verbose_name_plural = 'categories'
 
-    def __str__(self):
-        return self.cat_name
-
     def get_url(self):
         return reverse('products_by_category', args=[self.slug])
+
+    def __str__(self):
+        return self.cat_name
     
