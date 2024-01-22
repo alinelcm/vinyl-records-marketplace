fe38b4748ff9da215207f86f7741bfdc362838e1
Aline Miranda <aline.lcmiranda@gmail.com>
2024-01-22 14:57:45 -0300

    Register Form Created

--- store/views.py
+++ store/views.py
@@ -45,3 +45,17 @@ def product_detail(request, category_slug, product_slug):
         'in_cart':in_cart,
     }
     return render(request, 'store/product_detail.html', context)
+
+def search(request):
+    if 'keyword' in request.GET:
+        keyword = request.GET['keyword']
+        if keyword:
+            products = Product.objects.order_by('-created_date').filter(product_name__icontains=keyword)
+            product_count = products.count()
+    
+    context={
+        'products':products,
+        'product_count':product_count,
+    }
+    return render(request, 'store/store.html', context)
+    
